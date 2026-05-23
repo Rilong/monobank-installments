f81cf30c180b9897898ab853995fa69ed12dea10
Roman Hnatiuk <romgnatyuk@gmail.com>
2026-05-23 12:54:53 +0200

    refactor: replace if block with array_filter in ReturnOrderDTO::toArray

--- src/DTOs/ReturnOrderDTO.php
+++ src/DTOs/ReturnOrderDTO.php
@@ -16,17 +16,12 @@ readonly class ReturnOrderDTO
 
     public function toArray(): array
     {
-        $data = [
+        return array_filter([
             'order_id' => $this->orderId,
             'sum' => $this->sum,
             'store_return_id' => $this->storeReturnId,
             'return_money_to_card' => $this->returnMoneyTo === ReturnMoneyTo::Card,
-        ];
-
-        if ($this->additionalParams !== null) {
-            $data['additional_params'] = $this->additionalParams->toArray();
-        }
-
-        return $data;
+            'additional_params' => $this->additionalParams?->toArray(),
+        ], fn($v) => $v !== null);
     }
 }
